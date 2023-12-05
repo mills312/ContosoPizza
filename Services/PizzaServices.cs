7ae234b4f3b33f41f97057a2b69b3d4d150e8f0e
kevin mills <mills312@gmail.com>
2023-12-05 17:03:26 -0500

    Pizza Services Update Delete

--- Services/PizzaServices.cs
+++ Services/PizzaServices.cs
@@ -19,4 +19,21 @@ public static class PizzaService {
         pizza.Id = nextId++;
         Pizzas.Add(pizza);
     }
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if(pizza is null)
+            return;
+
+        Pizzas.Remove(pizza);
+    }
+
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+        if(index == -1)
+            return;
+
+        Pizzas[index] = pizza;
+    }
 }   
