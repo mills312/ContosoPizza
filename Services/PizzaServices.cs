838271e089b0344875ef1f519351e1cca2c430a9
kmills <mills312@gmail.com>
2023-12-05 15:05:15 -0500

    model

---
+++ Services/PizzaServices.cs
@@ -0,0 +1,22 @@
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Services;
+
+public static class PizzaService {
+    static List<Pizza> Pizzas { get; }
+    static int nextId = 3;
+    static PizzaService()
+    {
+        Pizzas = new List<Pizza>
+        {
+            new Pizza { Id = 1, Name = "Classic Italian", IsGlutenFree = false },
+            new Pizza { Id = 2, Name = "Vegie", IsGlutenFree = false }
+        };
+    }
+    public static List<Pizza> GetAll() => Pizzas;
+    public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+    public static void Add(Pizza pizza){
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+}   
