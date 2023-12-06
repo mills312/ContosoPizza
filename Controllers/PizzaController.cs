8b89894460bff1a2fcf4bd3bdf277e6841d948d3
kmills <mills312@gmail.com>
2023-12-06 10:25:05 -0500

    controller verbs

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -15,4 +15,24 @@ public class PizzaController : ControllerBase
     [HttpGet]
     public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
 
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+        if(pizza == null)
+            return NotFound();
+
+        return pizza;
+    }
+
+    [HttpPost]
+    public IActionResult Create(Pizza pizza){
+    }
+
+    [HttpPut("{id}"))]
+    public IActionResult Update(int id, Pizza pizza){}
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id){}
+
 }
