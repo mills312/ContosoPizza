bb21d242a4f7cd45ea4499420c77830ca05906ed
kevin mills <mills312@gmail.com>
2023-12-06 12:07:00 -0500

    service verbs details

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -27,12 +27,34 @@ public class PizzaController : ControllerBase
 
     [HttpPost]
     public IActionResult Create(Pizza pizza){
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
     }
 
-    [HttpPut("{id}"))]
-    public IActionResult Update(int id, Pizza pizza){}
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza){
+        if(id != pizza.Id)
+            return BadRequest();
+        
+        var existingPizza = PizzaService.Get(id);
+        if(existingPizza is null)
+            return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
 
     [HttpDelete("{id}")]
-    public IActionResult Delete(int id){}
+    public IActionResult Delete(int id){
+        var pizza = PizzaService.Get(id);
+
+        if(pizza is null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
 
 }
